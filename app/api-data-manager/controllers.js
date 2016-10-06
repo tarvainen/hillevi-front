525b6906c3e732866716877dd5fd0905231dc25a
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-10-07 00:28:16 +0300

    Fetch api selections to the api data manager interface.

--- app/api-data-manager/controllers.js
+++ app/api-data-manager/controllers.js
@@ -6,4 +6,55 @@
      */
     angular.module('ApiDataManager.Controllers', []);
 
+    /**
+     * Controller initializations.
+     */
+    angular.module('ApiDataManager.Controllers')
+        .controller('ApiDataManager.MainController', MainController)
+    ;
+
+    //////////////////
+
+    MainController.$inject = ['api', '$toast'];
+
+    /**
+     * Main controller for the api data manager interface.
+     *
+     * @param {*}   api
+     * @param {*}   $toast
+     *
+     * @constructor
+     */
+    function MainController (api, $toast) {
+        var vm = this;
+
+        /**
+         * Function for fetching APIs for the select input.
+         */
+        vm.fetchApis = function fetchApis () {
+            vm.apis = [];
+            vm.selectedApi = null;
+
+            api.route('interface/all')
+                .then(onSuccess, onError)
+            ;
+
+            /**
+             * Called when the list of APIs is fetched successfully.
+             *
+             * @param {*}   data
+             */
+            function onSuccess (data) {
+                vm.apis = data.data;
+            }
+        };
+
+        /**
+         * Called when the data fetch is failed.
+         */
+        function onError () {
+            $toast('DATA_FETCH_FAILED');
+        }
+    }
+
 })();
--- app/applications.js
+++ app/applications.js
@@ -5,7 +5,7 @@
      * Application injections.
      */
     angular.module('Hillevi.Applications', [
-        'Login', 'Settings', 'Dashboard', 'ApiManager'
+        'Login', 'Settings', 'Dashboard', 'ApiManager', 'ApiDataManager'
     ]);
 
 })();
