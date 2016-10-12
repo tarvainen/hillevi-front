9379be22169ce62c96cb8ad2330b3f733766c9b6
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-10-12 22:05:25 +0300

    Implement simple notification list and dismissing operations.

--- app/notifications/controllers.js
+++ app/notifications/controllers.js
@@ -15,13 +15,89 @@
 
     //////////////////////
 
+    MainController.$inject = ['api', '$toast'];
+
     /**
      * The main controller for the notifications interface.
      *
+     * @param {*}   api
+     * @param {*}   $toast
+     *
      * @constructor
      */
-    function MainController () {
+    function MainController (api, $toast) {
         var vm = this;
+
+        /**
+         * Loads the notification data.
+         */
+        vm.load = function load () {
+            api.route('notifications/latest/10')
+                .then(onSuccess)
+                .finally(onDone)
+            ;
+
+            /**
+             * Fired when the data is fetched from the server.
+             *
+             * @param {*}   data
+             */
+            function onSuccess (data) {
+                vm.data = data.data;
+            }
+        };
+
+        /**
+         * Selects all the elements in the interface.
+         */
+        vm.selectAll = function selectAll () {
+            vm.data.forEach(function (notification) {
+                notification.selected = true;
+            });
+        };
+
+        /**
+         * Dismiss all the selected notifications. After the
+         * operation we will fetch the data again.
+         */
+        vm.dismiss = function dismiss () {
+            var selected = [];
+
+            vm.data.forEach(function (notification) {
+                if (notification.selected) {
+                    selected.push(notification.id);
+                }
+            });
+
+            if (selected.length <= 0) {
+                return;
+            }
+
+            var params = {
+                id: selected
+            };
+
+            api.route('notifications/dismiss', params)
+                .then(onSuccess)
+                .finally(onDone)
+            ;
+
+            /**
+             * Fired when the notifications are successfully dismissed.
+             */
+            function onSuccess () {
+                $toast('NOTIFICATIONS_DISMISSED_SUCCESSFULLY');
+
+                vm.load();
+            }
+        };
+
+        /**
+         * The generic finally called method to close all loaders.
+         */
+        function onDone () {
+            vm.loading = false;
+        }
     }
 
 })();
